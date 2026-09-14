db3fb296300d8516e0c2e5d476bbf5fd18912a2e
Emanuel Ramayo <emanuelramayo12@gmail.com>
2026-09-14 17:38:39 -0300

    feat: configurar rutas CRUD artesanos

--- src/routes/artesanosRoutes.js
+++ src/routes/artesanosRoutes.js
@@ -1,12 +1,19 @@
 import express from "express";
 import {
   listarArtesanos,
-  obtenerArtesano
+  obtenerArtesano,
+  crearArtesano,
+  actualizarArtesano,
+  eliminarArtesano 
 } from "../controllers/artesanosController.js";
+import { validarId } from "../middlewares/validarId.js";    
 
 const router = express.Router();
 
 router.get("/", listarArtesanos);
-router.get("/:id", obtenerArtesano);
+router.post("/", crearArtesano);
+router.get("/:id", validarId, obtenerArtesano);
+router.put("/:id", validarId, actualizarArtesano);
+router.delete("/:id", validarId, eliminarArtesano);
 
 export default router;
