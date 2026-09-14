d82de652645a538eb773f8c5daadea2b931e1874
Emanuel Ramayo <emanuelramayo12@gmail.com>
2026-09-14 17:52:44 -0300

    feat:actualizando CRUD de productos

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,19 +1,59 @@
 import productos from "../data/productos.js";
+import { crearError } from "../utils/errores.js";
 
+//GET - /api/productos
 export const listarProductos = (req, res) => {
   res.json(productos);
 };
-
-export const obtenerProducto = (req, res) => {
-  const id = Number(req.params.id);
+//GET - /api/productos/:id
+export const obtenerProducto = (req, res, next) => {
+  const id = req.id;
 
   const producto = productos.find((producto) => producto.id === id);
 
   if (!producto) {
-    return res.status(404).json({
-      error: "Producto no encontrado"
-    });
+    return next(crearError("Producto no encontrado", 404));
+  }
+  res.json(producto);
+};
+
+//POST - /api/productos
+export const crearProducto = (req, res, next) => {
+  const { nombre, categoria, precio, artesanoId } = req.body;
+  if (!nombre || !categoria || !precio || !artesanoId) {
+    return next(crearError("Todos los campos son obligatorios", 400));
+  }
+  const nuevoProducto = { id: nextId(), nombre, categoria, precio, artesanoId };
+  productos.push(nuevoProducto);
+  res.status(201).json(nuevoProducto);
+};
+
+//PUT - /api/productos/:id
+export const actualizarProducto = (req, res, next) => {
+  const producto = productos.find((producto) => producto.id === req.id);
+  if (!producto) {
+    return next(crearError("Producto no encontrado", 404));
+  }
+  const { nombre, categoria, precio, artesanoId } = req.body;
+  if (!nombre || !categoria || !precio || !artesanoId) {
+    return next(crearError("Todos los campos son obligatorios", 400));
+  }
+  producto.nombre = nombre;
+  producto.categoria = categoria;
+  producto.precio = precio;
+  producto.artesanoId = artesanoId;
+  res.json(producto);
+  res.status(200).json({ message: "Producto actualizado correctamente" });
+};
+
+//DELETE - /api/productos/:id
+export const eliminarProducto = (req, res, next) => {
+  const index = productos.findIndex((producto) => producto.id === req.id);
+
+  if (index === -1) {
+    return next(crearError("Producto no encontrado", 404));
   }
 
-  res.json(producto);
+  productos.splice(index, 1);
+  res.status(204).send();
 };
