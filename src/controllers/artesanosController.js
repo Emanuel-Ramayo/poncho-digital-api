8127dac89104c841ba7472722efd65e8e3e70ca7
Emanuel Ramayo <emanuelramayo12@gmail.com>
2026-09-14 17:36:30 -0300

    feat:completar CRUD de artesanos

--- src/controllers/artesanosController.js
+++ src/controllers/artesanosController.js
@@ -1,19 +1,64 @@
 import artesanos from "../data/artesanos.js";
+import { crearError } from "../utils/errores.js";
 
+//GET - /api/artesanos
 export const listarArtesanos = (req, res) => {
   res.json(artesanos);
 };
 
-export const obtenerArtesano = (req, res) => {
-  const id = Number(req.params.id);
+//GET - /api/artesanos/:id
+export const obtenerArtesano = (req, res,next) => {
+  const id = req.id;
 
   const artesano = artesanos.find((artesano) => artesano.id === id);
 
   if (!artesano) {
-    return res.status(404).json({
-      error: "Artesano no encontrado"
-    });
+    return next(crearError("Artesano no encontrado", 404));
   }
-
   res.json(artesano);
 };
+
+//POST - /api/artesanos
+export const crearArtesano = (req, res,next) => {
+  const { nombre, especialidad, localidad } = req.body;
+
+  if (!nombre || !especialidad || !localidad) {
+    return next(crearError("Todos los campos son obligatorios", 400));
+  }
+  const nuevoArtesano = {id:nextId(), nombre, especialidad, localidad};
+  artesanos.push(nuevoArtesano);
+  res.status(201).json(nuevoArtesano);      
+}
+
+//PUT - /api/artesanos/:id
+export const actualizarArtesano = (req, res,next) => {
+  const artesano = artesanos.find((artesano) => artesano.id === req.id);
+
+  if (!artesano) {
+    return next(crearError("Artesano no encontrado", 404));
+  }
+  const { nombre, especialidad, localidad } = req.body;
+
+  if (!nombre || !especialidad || !localidad) {
+    return next(crearError("Todos los campos son obligatorios", 400));
+  } 
+  artesano.nombre = nombre;
+  artesano.especialidad = especialidad;
+  artesano.localidad = localidad;
+  res.json(artesano);
+  res.status(200).json({ message: "Artesano actualizado correctamente" });
+
+
+}
+//DELETE - /api/artesanos/:id
+export const eliminarArtesano = (req, res, next) => {
+    const index = artesanos.findIndex((artesano) => artesano.id === req.id);
+
+    if (index === -1) {
+        return next(crearError("Artesano no encontrado", 404));
+    }
+
+    artesanos.splice(index, 1);
+    res.status(204).send();
+};
+
